08d8a54a7ed1e3f2f6d0d345a4dad69b6964973e
StoryChara <mjarah@unal.edu.co>
2024-11-19 19:47:27 -0500

    Mover personaje para esquivar

--- Fighter.js
+++ Fighter.js
@@ -1,20 +1,38 @@
 class Fighter {
-  constructor(name, health, attack) {
+  constructor(name, health, attack, x, y) {
     this.name = name;
     this.health = health;
     this.attack = attack;
+    this.x = x;
+    this.y = y;
+    this.width = 50;
+    this.height = 100;
+    this.offset = 0;
+    this.moveSpeed = 50;
   }
 
-  takeDamage(damage) {
-    this.health -= damage;
-    if (this.health < 0) this.health = 0;
+  draw() {
+    fill(255, 0, 0);  
+    rect(this.x + this.offset, this.y, this.width, this.height);  // Dibuja el rectángulo en la posición con el offset
   }
 
-  isAlive() {
-    return this.health > 0;
+  moveLeft() {
+    if (this.offset === 0) {
+      this.offset = -this.moveSpeed;
+    }
   }
 
-  attackOpponent(opponent) {
-    opponent.takeDamage(this.attack);
+  moveRight() {
+    if (this.offset === 0) {
+      this.offset = this.moveSpeed; 
+    }
+  }
+
+  moveCenter(){
+    this.offset = 0;
+  }
+
+  update() {
+    this.draw();
   }
 }
--- Punch_Out__.js
+++ Punch_Out__.js
@@ -3,6 +3,7 @@ let pixelFont;
 let score;
 let menu;
 let angle = 0;
+let fighter1;
 
 function preload() {
   scenario = loadImage('sprites/scenario.png');
@@ -13,6 +14,7 @@ function preload() {
 function setup() {
   createCanvas(500, 500);
   menu = 0; 
+  fighter1 = new Fighter("Fighter 1", 100, 10, width / 2 - 25, height - 120);
 }
 
 function draw() {
@@ -33,12 +35,23 @@ function fight_menu() {
   background(18, 18, 18);
   animation_img(audience);
   image(scenario, 0, 0);
+  fighter1.update();
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
-    menu = 1;
+    menu = 1; 
   } else if (key === 'R' || key === 'r') {
-    menu = 0;
+    menu = 0; 
+  } else if (keyCode === LEFT_ARROW) {
+    fighter1.moveLeft(); 
+  } else if (keyCode === RIGHT_ARROW) {
+    fighter1.moveRight(); 
+  }
+}
+
+function keyReleased() {
+  if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW) {
+    fighter1.moveCenter();  
   }
 }
